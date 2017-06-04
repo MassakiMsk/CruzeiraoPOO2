38150c8335387f872d8de6e4aa3b6e7d08c27bcc
Massaki Msk <alisonmassakicheno@hotmail.com>
2017-06-04 19:12:14 -0300

    Tentando cadastrar campeonato

---
+++ Cruzeirao/src/main/java/sistema/Service/CampeonatoService.java
@@ -0,0 +1,35 @@
+package sistema.Service;
+
+import java.util.List;
+
+import sistema.Dao.CampeonatoDAO;
+import sistema.modelos.Campeonato;
+
+
+public class CampeonatoService {
+	CampeonatoDAO champDAO = new CampeonatoDAO();
+
+	public Campeonato save(Campeonato campeonato){
+		campeonato = champDAO.salvar(campeonato);
+		champDAO.closeEntityManager();
+		return campeonato;
+	}
+
+	public List <Campeonato> getChamps()
+	{
+		List <Campeonato> list =  champDAO.getAll(Campeonato.class);
+		champDAO.closeEntityManager();
+		return list;
+	}
+	public void update(Campeonato campeonato)
+	{
+		champDAO.salvar(campeonato);
+		champDAO.closeEntityManager();
+	}
+	public void remove(Campeonato campeonato)
+	{
+		campeonato = champDAO.getById(Campeonato.class, campeonato.getCodigoCampeonato());
+		champDAO.remover(campeonato);
+		champDAO.closeEntityManager();
+	}	
+}
